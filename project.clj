9aa11a3475248c1e980d6354591df3150e421dcf
Ivan Grishaev <ivan@grishaev.me>
2023-03-08 18:58:51 +0300

    bump dynamo

--- project.clj
+++ project.clj
@@ -51,7 +51,7 @@
    :ydb
    {:main ^:skip-aot teleward.bash
     :dependencies
-    [[com.github.igrishaev/dynamodb "0.1.1"]]}
+    [[com.github.igrishaev/dynamodb "0.1.4"]]}
 
    :uberjar
    {:aot :all
